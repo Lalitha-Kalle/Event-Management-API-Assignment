99c791f7cd4ea4663343c967527935c8f277b7b8
Lalitha-Kalle <lalithaprogramming@gmail.com>
2025-07-15 12:47:43 +0530

    Registration model added

---
+++ src/migrations/20250715070939-create-registration.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Registrations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: 'id'
+        },
+        onDelete: 'CASCADE' //i added this constraint because if any user is deleted then it remove registration of that user
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Events',
+          key: 'id'
+        },
+        onDelete: 'CASCADE' //same here if event is deleted then it remove registration of that event
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Registrations');
+  }
+};
