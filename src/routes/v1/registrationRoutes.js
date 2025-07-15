cbea84d5f9a8fd7aa0c949220def07890819b9ca
Lalitha-Kalle <lalithaprogramming@gmail.com>
2025-07-15 23:12:28 +0530

    added registration routes

--- src/routes/v1/registrationRoutes.js
+++ src/routes/v1/registrationRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser, cancelRegistration } = require("../../controllers/registrationControllers")
 
 // Register a user to an event
-router.post('/events/:eventId/register', (req, res) => {});
+router.post('/events/:eventId/register', registerUser);
 
 // Cancel a user’s registration
-router.delete('/events/:eventId/register', (req, res) => {});
+router.delete('/events/:eventId/register', cancelRegistration);
 
 module.exports = router;
